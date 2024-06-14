c1f62d5d6eac9f43576d8ab851425fb4d388a676
Jocelyn Atwell <jocelyn@bitcube.tech>
2024-06-14 15:54:05 +0200

    Complete Sass webpage, but lacking in mobile responsiveness

--- gulpfile.js
+++ gulpfile.js
@@ -1,16 +1,18 @@
 const { src, dest, watch, series } = require("gulp");
 const sass = require('gulp-sass')(require("sass"));
+const purgecss = require("gulp-purgecss");
 
 //function used to compile sass into css
 function buildStyles() {
-    return src("shinobi/**/*.scss")
+    return src("sass/**/*.scss")
         .pipe(sass())
+        // .pipe(purgecss({ content: ["*.html"]}))
         .pipe(dest("css"))
 }
 
 //function to watch the source file and automatically run the buildStyles function
 function watchTask() {
-    watch(["shinobi/**/*.scss"], buildStyles)
+    watch(["sass/**/*.scss", "*.html"], buildStyles)
 }
 
 exports.default = series(buildStyles, watchTask);
